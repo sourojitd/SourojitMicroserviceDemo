b23e582a2c69f3d6f0b844ca3c4a7ce57644c62f
Sourojit <soft.jd4u@gmail.com>
2021-07-04 01:30:11 +0530

    This is a new commit for what I originally planned to be amended

---
+++ user-service/src/main/java/com/dailycodebuffer/user/VO/ResponseTemplateVO.java
@@ -0,0 +1,15 @@
+package com.sourojitMSdemo.user.VO;
+
+import com.sourojitMSdemo.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private User user;
+    private Department department;
+}
